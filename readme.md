252809a197ebf04e33a70050ade23e9c898234fd
momoNorthy <northy360@126.com>
2019-02-18 12:02:57 +0800

    momo's life and codes all in this website.

---
+++ readme.md
@@ -0,0 +1,2 @@
+# this is momo's website.
+## web address https://github.com/momoNorthy/momoNorthy.local.sub.io
---
+++ submit-website.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+git add .
+git commit -m "momo's life and codes all in this website."
+git push
+echo success.
+pause
