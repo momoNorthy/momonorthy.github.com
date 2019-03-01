17740d5a1f568c5d246ea752eb02021f0c624463
momoNorthy <northy360@126.com>
2019-03-01 19:40:45 +0800

    拷贝到D:/盘

---
+++ ppt-tools/.cxx
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+#include<windows.h>
+int wmain()
+{
+	SetConsoleTitleA("检测一下");
+	std::cout<<"长征时间是由（）开始"<<"\b\b\b\b"<<std::endl;
+	int a;
+	std::cin>>a;std::cin.get();
+	if(a==1934) std::cout<<"对啦！"<<std::endl;
+	else std::cout<<"小子，还需努力噢"<<std::endl;
+	std::cin.get();
+	std::system("cls");
+	std::cout<<"什么时候结束？"<<std::endl;
+	std::cin>> a;
+	if (a==1936) std::cout<<"挺6"<<std::endl;
+	else std::cout<<"错啦，小子"<<std::endl;
+	std::cin.get();
+	std::cin.get();
+}
---
+++ ppt-tools/tools1.source.cxx
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<conio.h>
+#include<cstdlib>
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+int main(int argc, char** argv) {
+std::cout<<"举手！谢谢！"<<std::endl; 
+std::cout<<"中国人受到了哪次最大的伤害？"<<std::endl;
+std::cout<<"a.南京大屠杀"<<" b.刚刚入侵"<<std::endl;
+char a=_getch();
+std::system("cls");
+if (a=='a' || a=='A')std::cout<<"好棒呦！！！"<<std::endl;
+else std::cout<<"小伙子，要努力哦"<<std::endl;\
+_getch();
+std::system("cls");
+std::cout<<"课外题：共产党成立于____年。\b\b"<<std::endl;
+std::cout<<"a.1921 b.1931 c.1941 d.1911"<<std::endl;
+char b=_getch();
+if (b=='a'||b=='A') std::cout<<"好棒呦！！！"<<std::endl;
+else std::cout<<"小伙子，要努力哦"<<std::endl;
+_getch();
+return 0;
+}
